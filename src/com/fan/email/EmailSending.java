bc36a12a9bf267df89ee281c4945ebae45b7c1a8
yorktips <york.tips@gmail.com>
2019-01-14 00:57:47 -0500

    test
    test

--- src/com/fan/email/EmailSending.java
+++ src/com/fan/email/EmailSending.java
@@ -1,26 +1,64 @@
 package com.fan.email;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Properties;
+
 import com.fan.email.ScheduledEmailBatchSender;
 
+//Useage:
+//java com.fan.email.EmailSending
 public class EmailSending {
 	// JDBC driver name and database URL
-	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
-	static final String DB_URL = "jdbc:mysql://localhost/demodb";
+	static String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+	static String DB_URL = "jdbc:mysql://localhost/central";
 
 	// Database credentials
-	static final String USER = "root";
-	static final String PASS = "";
+	static String USER = "root";
+	static String PASS = "";
 
 	public static void main(String[] args) {
+		Properties prop = new Properties();
+		OutputStream output = null;
+		InputStream input = null;
+
 		try {
-			System.out.println("main start" );
-			ScheduledEmailBatchSender scheduledEmailBatchSender= new ScheduledEmailBatchSender(JDBC_DRIVER,DB_URL,USER,PASS);
-			System.out.println("thread start" );
-			scheduledEmailBatchSender.start();
-			System.out.println("main end" );
-		}catch(Exception e) {
-			e.printStackTrace();
+			input = new FileInputStream("./Database.properties");
+
+			// load a properties file
+			prop.load(input);
+			// get the property value and print it out
+			DB_URL=prop.getProperty("DBURL");
+			USER=prop.getProperty("USERNAME");
+			PASS=prop.getProperty("PASSWORDW");
+			System.out.println("DB_URL=" + DB_URL + ";USER=" + USER + ";PASS=" + PASS);
+			
+			try {
+				System.out.println("main start" );
+				ScheduledEmailBatchSender scheduledEmailBatchSender= new ScheduledEmailBatchSender(JDBC_DRIVER,DB_URL,USER,PASS);
+				System.out.println("thread start" );
+				scheduledEmailBatchSender.start();
+				System.out.println("main end" );
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
+			
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		} finally {
+			if (input != null) {
+				try {
+					input.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
 		}
+		
+
 	}
 
 }
